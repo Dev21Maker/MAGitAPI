465c5bbb4d2059f7f064ae3a43e61ba043ace544
Dev21Maker <152169910+Dev21Maker@users.noreply.github.com>
2024-05-23 19:22:25 +0200

    add navigation functionality and button click to script

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-
+async function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time);
+  });
+}
 // Use the stealth plugin
 puppeteer.use(StealthPlugin());
 
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://google.com');
   // Your puppeteer code here
+  await page.waitForSelector("#L2AGLb > div");
+  await delay(200);
+  await page.click("#L2AGLb > div");
+
+  await page.waitForSelector("#gb > div > div.gb_Ud > a");
+  await delay(200);
+  await page.click("#gb > div > div.gb_Ud > a");
+
+  
   await page.screenshot({ path: 'screenshot.png' });
   await browser.close();
 })();
